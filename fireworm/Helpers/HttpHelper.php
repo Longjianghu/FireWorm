1fbf1ca409366a8726918761a75f2eae106dfb34
longjianghu <215241062@qq.com>
2019-02-19 09:25:22 +0800

    更新HttpHelper

--- fireworm/Helpers/HttpHelper.php
+++ fireworm/Helpers/HttpHelper.php
@@ -15,44 +15,112 @@
 
 namespace Fireworm\Helpers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
 class HttpHelper
 {
     /**
      * GET 请求
      *
      * @access public
-     * @param  string $url  URL
-     * @param  array  $args 提交参数
-     * @return mixed
+     * @param  string $url     URL
+     * @param  array  $args    提交参数
+     * @param  array  $headers HEAD信息
+     * @return array
+     * @throws
      */
-    public static function get(string $url, array $args = [])
+    public static function get(string $url, array $args = [], array $headers = [])
     {
+        $status = ['code' => 0, 'data' => [], 'msg' => ''];
 
+        try {
+            $options = ( ! empty($args)) ? ['query' => $args] : [];
+
+            if ( ! empty($headers)) {
+                $options['headers'] = $headers;
+            }
+
+            $response = (new Client())->request('GET', $url, $options);
+
+            $status = ['code' => 200, 'data' => $response->getBody()->getContents(), 'msg' => ''];
+        } catch (RequestException $e) {
+            $status['msg'] = $e->getMessage();
+        }
+
+        return $status;
     }
 
     /**
      * POST 请求
      *
      * @access public
-     * @param  string $url  URL
-     * @param  array  $args 提交参数
-     * @return mixed
+     * @param  string $url     URL
+     * @param  array  $args    提交参数
+     * @param  array  $headers HEAD信息
+     * @return array
+     * @throws
      */
-    public static function post(string $url, array $args = [])
+    public static function post(string $url, array $args = [], array $headers = [])
     {
+        $status = ['code' => 0, 'data' => [], 'msg' => ''];
 
+        try {
+            $options = ( ! empty($args)) ? ['form_params' => $args] : [];
+
+            if ( ! empty($headers)) {
+                $options['headers'] = $headers;
+            }
+
+            $response = (new Client())->request('POST', $url, $options);
+
+            $status = ['code' => 200, 'data' => $response->getBody()->getContents(), 'msg' => ''];
+        } catch (RequestException $e) {
+            $status['msg'] = $e->getMessage();
+        }
+
+        return $status;
     }
 
     /**
      * 文件上传
      *
      * @access public
-     * @param  string $url  URL
-     * @param  array  $args 提交参数
-     * @return mixed
+     * @param  string $url      URL
+     * @param  string $filename 上传文件
+     * @param  array  $headers  HEAD信息
+     * @return array
+     * @throws
      */
-    public static function upload(string $url, array $args = [])
+    public static function upload(string $url, string $filename, array $headers = [])
     {
+        $status = ['code' => 0, 'data' => [], 'msg' => ''];
 
+        try {
+            if ( ! is_file($filename)) {
+                throw new RequestException('上传文件不存在！');
+            }
+
+            $options = [
+                'multipart' => [
+                    [
+                        'name'     => 'file',
+                        'contents' => file_get_contents($filename),
+                        'filename' => basename($filename)
+                    ]
+                ]
+            ];
+
+            if ( ! empty($headers)) {
+                $options['headers'] = $headers;
+            }
+
+            $response = (new Client())->request('POST', $url, $options);
+            $status   = ['code' => 200, 'data' => $response->getBody()->getContents(), 'msg' => ''];
+        } catch (RequestException $e) {
+            $status['msg'] = $e->getMessage();
+        }
+
+        return $status;
     }
 }
